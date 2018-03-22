33f71f54e4c7fbd7539b76fa1f77913d1e9a5454
Alex Castillo <alex@castillo.io>
2018-03-22 14:50:16 -0700

    (feat): add balls with sound
    Adds a ball for each note played from controller

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { UserPresenceService, IUser } from './user-presence.service';
 import { Component, OnInit } from '@angular/core';
+import { UserPresenceService, IUser } from './user-presence.service';
+import { FirebaseNotesService, INoteEvent } from './firebase-notes.service';
+
+const cdnUrl = 'https://cdn.glitch.com/ed38cda4-8b9e-460f-83fa-3c9f7ed0bf7e';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +13,27 @@ export class AppComponent implements OnInit {
 
   users: IUser[];
 
-  constructor(private userPresence: UserPresenceService) {
+  balls: INoteEvent[] = [];
+
+  sounds = {
+    throw: { src: `${cdnUrl}/sfx_throw.wav?1521187676351` },
+    jump: { src: `${cdnUrl}/jump.ogg?1521187674201` }
+  };
+
+  constructor(
+    private userPresence: UserPresenceService,
+    private notes: FirebaseNotesService
+  ) {
     userPresence.users$.subscribe((users) => {
       this.users = Object.values(users);
     });
+    notes.notes$.subscribe(note => {
+      this.balls.push(note);
+    });
+  }
+
+  removeBall(ball) {
+    this.balls = this.balls.filter(b => b !== ball);
   }
 
   ngOnInit() {
