dcf3710d2552ce18fccf170d632487acafa9a86b
Uri Shaked <uri@urishaked.com>
2018-03-27 01:25:50 +0300

    feat: add funky dance floor

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { environment } from './environments/environment';
 import 'aframe-fireball-component';
 import 'aframe-particle-system-component';
 import 'aframe-animation-component';
+import 'aframe-gif-shader';
 
 if (environment.production) {
   enableProdMode();
