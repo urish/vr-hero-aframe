6c2033ee225ff512a127ff9d83d9007178d56c7e
Uri Shaked <uri@urishaked.com>
2018-04-18 13:34:50 -0600

    fix: import the aframe pipe correctly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AframePipe } from 'angular-aframe-pipe';
+import { AframePipeModule } from 'angular-aframe-pipe';
 import { JumpDetectionService } from './jump-detection.service';
 import { FirebaseUtilsService } from './firebase-utils.service';
 import { ControllerService } from './controller.service';
 import { UserPresenceService } from './user-presence.service';
 
 @NgModule({
-  declarations: [AppComponent, AframePipe],
-  imports: [BrowserModule],
-  providers: [JumpDetectionService, FirebaseUtilsService, ControllerService, UserPresenceService],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AframePipeModule],
+  providers: [JumpDetectionService, FirebaseUtilsService, ControllerService, UserPresenceService, AframePipeModule],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
