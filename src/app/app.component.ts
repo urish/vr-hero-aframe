9b1d58c7b93345a56e2f72a35114d8c9828bf84c
Uri Shaked <uri@urishaked.com>
2018-04-18 15:30:55 -0600

    cleanup: start from scratch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,60 +1,10 @@
-import { JumpDetectionService } from './jump-detection.service';
-import { Component, OnInit } from '@angular/core';
-import { UserPresenceService, IUser } from './user-presence.service';
-import { ControllerService, IControllerEvent } from './controller.service';
-
-const cdnUrl = 'https://cdn.glitch.com/ed38cda4-8b9e-460f-83fa-3c9f7ed0bf7e';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  users: IUser[];
-  me: IUser;
-
-  balls: IControllerEvent[] = [];
-
-  sounds = {
-    throw: { src: `${cdnUrl}/sfx_throw.wav?1521187676351` },
-    jump: { src: `${cdnUrl}/jump.ogg?1521187674201` },
-  };
-
-  constructor(
-    private userPresence: UserPresenceService,
-    controller: ControllerService,
-    jumpDetectionService: JumpDetectionService,
-  ) {
-    userPresence.me$.subscribe((user) => {
-      this.me = user;
-    });
-    userPresence.users$.subscribe((users) => {
-      this.users = users;
-    });
-    controller.pose$.subscribe((pose) => {
-      this.balls.push(pose);
-    });
-    jumpDetectionService.jumps$.subscribe((jumpValue) => {
-      userPresence.setJumping(jumpValue);
-    });
-  }
-
-  removeBall(ball: IControllerEvent) {
-    this.balls = this.balls.filter((b) => b !== ball);
-  }
-
-  ngOnInit() {}
-
-  userId(user: IUser) {
-    return user.id;
-  }
-
-  onRotationChanged(e: AFrame.DetailEvent<AFrame.Coordinate>) {
-    this.userPresence.updateMyRotation(e.detail);
-  }
-
-  onPositionChanged(e: AFrame.DetailEvent<AFrame.Coordinate>) {
-    this.userPresence.updateMyPosition(e.detail);
-  }
+export class AppComponent {
+  title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AframePipeModule } from 'angular-aframe-pipe';
-import { JumpDetectionService } from './jump-detection.service';
-import { FirebaseUtilsService } from './firebase-utils.service';
-import { ControllerService } from './controller.service';
-import { UserPresenceService } from './user-presence.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AframePipeModule],
-  providers: [JumpDetectionService, FirebaseUtilsService, ControllerService, UserPresenceService, AframePipeModule],
   bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
